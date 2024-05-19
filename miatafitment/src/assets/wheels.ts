c9c00855501e99ebc5e6b3d32dad97abf608bc39
anthony <53194069+aanthoonyy@users.noreply.github.com>
2024-05-19 09:55:32 -0700

    wheel

--- miatafitment/src/assets/wheels.ts
+++ miatafitment/src/assets/wheels.ts
@@ -1,15 +1,23 @@
 import { camberSlider } from "./sliders/CamberSlider";
 
 export function makeWheels(THREE: any, x: number, y: number, z: number) {
-    const wheelGeometry = new THREE.CylinderGeometry(7/12, 7/12, 8/12, 32);
+    const wheelGeometry = new THREE.CylinderGeometry(7/12, 7/12, 0.01/12, 32);
     const wheelMaterial = new THREE.MeshBasicMaterial({color: 0xC0C0C0, transparent: true, opacity: 0.5});
     const wheel = new THREE.Mesh(wheelGeometry, wheelMaterial);
-    
-    // const test = new THREE.CylinderGeometry(7/12, 7/12, 4/12, 32);
-    // const testa = new THREE.MeshBasicMaterial({color: 0x00000, transparent: true, opacity: 0.5});
-    // const test2 = new THREE.Mesh(test, testa);
-    // test2.position.y = 2/12;   
-    // wheel.add(test2);
+
+    // Create two new cylinders
+    const sideCylinderGeometry = new THREE.CylinderGeometry(7/12, 7/12, 3/12, 32);
+    const sideCylinderMaterial = new THREE.MeshBasicMaterial({color: 0xC0C0C0, transparent: true, opacity: 0.5});
+
+    // Add the cylinders to each side of the wheel
+    const sideCylinder1 = new THREE.Mesh(sideCylinderGeometry, sideCylinderMaterial);
+    sideCylinder1.position.y = wheel.position.y + 1.6/12;
+    wheel.add(sideCylinder1);
+
+    const sideCylinder2 = new THREE.Mesh(sideCylinderGeometry, sideCylinderMaterial);
+    sideCylinder2.position.y = -1.6/12;
+    wheel.add(sideCylinder2);
+
     // Rotate the wheel 90 degrees around the x-axis
     wheel.rotation.x = Math.PI / 2;
     wheel.position.x = x;
