81b5eb637a78d7accbdeed4f014abef269f99969
anthony <53194069+aanthoonyy@users.noreply.github.com>
2025-01-02 15:39:15 -0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 import MainComponent from "./mainComponent";
 import { Route, Routes } from "react-router";
 import LandingPage from "./landingPage";
 import GalleryPage from "./galleryPage";
 import MarketplacePage from "./marketPage";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+
 import TodoList from "./todo";
 
 Amplify.configure(outputs);
